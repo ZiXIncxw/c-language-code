f7da3605e509d4afe2183f7f4c6536430633918f
chi-xuewen <1143436256@qq.com>
2025-09-01 21:06:49 +0800

    每日一份

---
+++ test_9.1/test_9.1/test.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//    double pie = 3.1415926;
+//    float r = 0.0;
+//    scanf("%f", &r);
+//    double V = 4.000 / 3.000 * pie * r * r * r;
+//    printf("%.3lf", V);
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int score1 = 0;
+//    int score2 = 0;
+//    int score3 = 0;
+//    scanf("%d %d %d", &score1, &score2, &score3);
+//    printf("score1=%d,score2=%d,score3=%d", score1, score2, score3);
+//
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//
+//{
+//    for (int i = 10000; i <= 99999; i++)
+//    {
+//        int sum = 0;
+//        for (int j = 10; j < 100000; j *= 10)
+//        {
+//            sum += (i % j) * (i / j);
+//        }
+//        if (sum == i)
+//        {
+//            printf("%d ", i);
+//        }
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    printf("%d\n", (a + b) % 100);
+//
+//    return 0;
+//}
+
+
+
+
+
+
+
+
