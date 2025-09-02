366004df136005f8ab6c57f94210b656b57f6b49
chi-xuewen <1143436256@qq.com>
2025-09-02 21:57:09 +0800

    666

---
+++ test_9.2/test_9.2/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int second = 0;
+//    scanf("%d", &second);
+//    printf("%d %d %d", second / 3600, second / 60 % 60, second % 60);
+//
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	float a = 0.0;
+//	float b = 0.0;
+//	float c = 0.0;
+//	scanf("%f %f %f", &a, &b, &c);
+//	float total = a + b + c;
+//	float av = total / 3.0;
+//	printf("%.2f %.2f", total, av);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
