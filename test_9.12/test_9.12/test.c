a5d1676a2f68534d8ab408ed1665a532bc771cae
chi-xuewen <1143436256@qq.com>
2025-09-12 21:34:45 +0800

    666

---
+++ test_9.12/test_9.12/test.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//    char o = 0;
+//    double n1 = 0.0;
+//    double n2 = 0.0;
+//    while (~scanf("%lf%c%lf", &n1, &o, &n2))
+//    {
+//        switch (o)
+//        {
+//        case '+':
+//            printf("%.4lf+%.4lf=%.4lf", n1, n2, n1 + n2);
+//            break;
+//        case '-':
+//            printf("%.4lf-%.4lf=%.4lf", n1, n2, n1 - n2);
+//            break;
+//        case '*':
+//            printf("%.4lf*%.4lf=%.4lf", n1, n2, n1 * n2);
+//            break;
+//        case '/':
+//            if (n2 != 0)
+//                printf("%.4lf/%.4lf=%.4lf", n1, n2, n1 / n2);
+//            else
+//                printf("Wrong!Division by zero!\n");
+//            break;
+//        default:
+//            printf("Invalid operation!\n");
+//            break;
+//        }
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n = 0;
+//    while (~scanf("%d", &n))
+//    {
+//        for (int i = 0; i < n; i++)
+//        {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//
+//    return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    while (~scanf("%d", &n))
+//    {
+//        for (int i = 0; i < n; i++)
+//        {
+//            for (int j = 0; j < n; j++)
+//            {
+//                printf("* ");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+#/*include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    while (~scanf("%d", &n))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                printf("* ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}*/
