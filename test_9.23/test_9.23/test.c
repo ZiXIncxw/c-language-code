2d715b5d983103d5e280cebabf46ec1fd56b2ae9
chi-xuewen <1143436256@qq.com>
2025-09-23 21:12:50 +0800

    死

---
+++ test_9.23/test_9.23/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    printf("%d\n", n / 12 * 4 + 2);
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    long long n = 0;
+//    long long m = 0;
+//
+//    long long tmp = 0;
+//    long long max = 0;
+//    scanf("%lld %lld", &n, &m);
+//    long long n1 = n;
+//    long long m1 = m;
+//
+//    while (tmp = m % n)
+//    {
+//        m = n;
+//        n = tmp;
+//
+//    }
+//    max = n;
+//    long long min = n1 * m1 / max;
+//    printf("%lld\n", max + min);
+//    return 0;
+//}
