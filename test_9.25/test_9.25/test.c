3ba82e87f80c26b1ed4e0586a961b90e6625959b
chi-xuewen <1143436256@qq.com>
2025-09-25 16:03:43 +0800

    c语言训练营完结撒花！

---
+++ test_9.25/test_9.25/test.c
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int count = 0;
+//    int i = 0;
+//    int j = 0;
+//    int n = 0;
+//    scanf("%d", &n);
+//    int arr[101] = { 0 };
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    int found = 0;
+//    scanf("%d", &found);
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (arr[i] == found)
+//        {
+//            count++;
+//        }
+//    }
+//    printf("%d\n", count);
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("printf(\"Hello world!\\n\");\n");
+//    printf("cout << \"Hello world!\" << endl;\n");
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int count = 0;
+//
+//    for (int i = 100; i < 1000; i++)
+//    {
+//        int prime = 1;
+//        for (int j = 2; j < i; j++)
+//        {
+//            if (i % j == 0)
+//            {
+//                prime = 0;
+//                break;
+//            }
+//        }
+//        if (prime == 1)
+//        {
+//            count++;
+//        }
+//    }
+//
+//    printf("%d\n", count);
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int m = 0;
+//    int arr[1001] = { 0 };
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &m);
+//        arr[m] = m;
+//    }
+//    for (int i = 0; i < 1001; i++)
+//    {
+//        if (arr[i] != 0)
+//        {
+//            printf("%d ", arr[i]);
+//        }
+//    }
+//
+//    return 0;
+//}
+
+
+
+
+
+
+
