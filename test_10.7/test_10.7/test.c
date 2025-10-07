d57022b72898808457ca86cfb515eda446f6f243
chi-xuewen <1143436256@qq.com>
2025-10-07 20:47:06 +0800

    指针

---
+++ test_10.7/test_10.7/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//void Print(char* str, int k,int len)
+//{
+//	int time = k % len;
+//	for (int i = 0; i < time; i++)
+//	{
+//		char tmp = str[0];
+//		int j;
+//		for (j = 0; j < len - 1; j++)
+//		{
+//			str[j] = str[j + 1];
+//
+//		}
+//		str[len-1] = tmp;
+//	}
+//}
+//
+//
+//int main()
+//{
+//
+//	char str[] = "abcdef";
+//	int len = strlen(str);
+//	Print(str, 4,len);
+//	printf("%s ", str);
+//	return 0;
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
