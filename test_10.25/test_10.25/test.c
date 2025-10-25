5e461731a843b862028110bbdf6a45805058617e
chi-xuewen <1143436256@qq.com>
2025-10-25 19:20:58 +0800

    sizeof与strlen

---
+++ test_10.25/test_10.25/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	char arr[] = { 'a','b','c','d','e','f' };
+//	printf("%d\n", strlen(arr));
+//	printf("%d\n", strlen(arr + 0));
+//	printf("%d\n", strlen(*arr));
+//	printf("%d\n", strlen(arr[1]));
+//	printf("%d\n", strlen(&arr));
+//	printf("%d\n", strlen(&arr + 1));
+//	printf("%d\n", strlen(&arr[0] + 1));
+//	return 0;
+//}
+
+int main()
+{
+	char arr[] = "abcdef";
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(arr + 0));
+	printf("%d\n", sizeof(*arr));
+	printf("%d\n", sizeof(arr[1]));
+	printf("%d\n", sizeof(&arr));
+	printf("%d\n", sizeof(&arr + 1));
+	printf("%d\n", sizeof(&arr[0] + 1));
+	return 0;
+}
+
+
+
+
+
+
+
