189753db998961b386157452b1f8716b3c5268ed
chi-xuewen <1143436256@qq.com>
2025-10-26 21:00:43 +0800

    sizeof与strlen

---
+++ test_10.26/test_10.26/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%d\n", strlen(arr));
+//	printf("%d\n", strlen(arr + 0));
+//	//printf("%d\n", strlen(*arr));
+//	//printf("%d\n", strlen(arr[1]));
+//	printf("%d\n", strlen(&arr));
+//	printf("%d\n", strlen(&arr + 1));
+//	printf("%d\n", strlen(&arr[0] + 1));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* p = "abcdef";
+//	printf("%d\n", sizeof(p));
+//	printf("%d\n", sizeof(p + 1));
+//	printf("%d\n", sizeof(*p));
+//	printf("%d\n", sizeof(p[0]));
+//	printf("%d\n", sizeof(&p));
+//	printf("%d\n", sizeof(&p + 1));
+//	printf("%d\n", sizeof(&p[0] + 1));
+//	return 0;
+//
+//}
+
+int main()
+{
+	char* p = "abcdef";
+	printf("%d\n", strlen(p));
+	printf("%d\n", strlen(p + 1));
+	printf("%d\n", strlen(*p));
+	printf("%d\n", strlen(p[0]));
+	printf("%d\n", strlen(&p));
+	printf("%d\n", strlen(&p + 1));
+	printf("%d\n", strlen(&p[0] + 1));
+	return 0;
+}
