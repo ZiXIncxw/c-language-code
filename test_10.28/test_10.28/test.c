479f8a84f1f43a9acd3c6d2f52c7a8ef359449f0
chi-xuewen <1143436256@qq.com>
2025-10-28 21:07:45 +0800

    少

---
+++ test_10.28/test_10.28/test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int a[3][4] = { 0 };
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(a[0][0]));
+//	printf("%d\n", sizeof(a[0]));
+//	printf("%d\n", sizeof(a[0] + 1));
+//	printf("%d\n", sizeof(*(a[0] + 1)));
+//	printf("%d\n", sizeof(a + 1));
+//	printf("%d\n", sizeof(*(a + 1)));
+//	printf("%d\n", sizeof(&a[0] + 1));
+//	printf("%d\n", sizeof(*(&a[0] + 1)));
+//	printf("%d\n", sizeof(*a));
+//	printf("%d\n", sizeof(a[3]));
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[5] = { 1, 2, 3, 4, 5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 1));
+//	return 0;
+//}
+
+//在X86环境下
+//假设结构体的??是20个字节
+//程序输出的结果是啥？
+
+struct Test
+{
+	int Num;
+	char* pcName;
+	short sDate;
+	char cha[2];
+	short sBa[4];
+}*p = (struct Test*)0x100000;
+int main()
+{
+	printf("%p\n", p + 0x1);
+	printf("%p\n", (unsigned long)p + 0x1);
+	printf("%p\n", (unsigned int*)p + 0x1);
+	return 0;
+}
