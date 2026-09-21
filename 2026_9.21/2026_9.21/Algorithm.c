11db47dd701bb4d750d2bb794a80f256b07558e2
ZiXIncxw <1143436256@qq.com>
2026-09-21 22:18:05 +0800

    添加 2026_9.21：四种排序整理版，修正堆排序循环边界（Algorithm.c）
    - 统一命名与格式：Test_InsertSort / Test_ShellSort / Test_SelectSort / Test_HeapSort
    - 希尔排序由 Test_ShellSort_1、Test_ShellSort_2 两版合并为一版
    - 堆排序 Test_HeapSort 的 while (end >= 0) 改为 while (end > 0)，去掉多余的一轮自交换
    - 选择排序保留 maxi == begin 时的修正（先换最小值，最大值下标要跟着改）
    - 打印函数 Print 移到文件上方
    - main 里换成最坏情况数据 { 10,9,8,7,6,5,4,3,2,1 } 做验证

---
+++ 2026_9.21/2026_9.21/Algorithm.c
@@ -0,0 +1,152 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void Print(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+void Swap(int* p1, int* p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+//{ 1,3,5,7,9,2,4,6,8,10 }
+void Test_InsertSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int end = i;
+		int tmp = arr[end + 1];
+		while (end >= 0)
+		{
+			if (arr[end] > tmp)
+			{
+				arr[end + 1] = arr[end];
+				end--;
+			}
+			else
+			{
+				break;
+			}
+		}
+		arr[end + 1] = tmp;
+	}
+}
+
+void Test_ShellSort(int* arr, int size)
+{
+	int gap = size;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = 0; i < size - gap; i++)
+		{
+			int end = i;
+			int tmp = arr[end + gap];
+			while (end >= 0)
+			{
+				if (arr[end] > tmp)
+				{
+					arr[end + gap] = arr[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			arr[end + gap] = tmp;
+		}
+	}
+}
+//{ 1,3,5,7,9,2,4,6,8,10 }
+
+//{ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }
+void Test_SelectSort(int* arr, int size)
+{
+	int begin = 0;
+	int end = size - 1;
+	while (begin < end)
+	{
+		int maxi = begin;
+		int mini = begin;
+		for (int i = begin+1; i <= end; i++)
+		{
+			if (arr[i] < arr[mini])
+			{
+				mini = i;
+			}
+			if (arr[i] > arr[maxi])
+			{
+				maxi = i;
+			}
+		}
+		Swap(&arr[begin], &arr[mini]);
+		if (maxi == begin)
+		{
+			maxi = mini;
+		}
+		Swap(&arr[end], &arr[maxi]);
+		begin++;
+		end--;
+	}
+}
+
+void AdjustDown(int* arr,int size,int parent)
+{
+	int child = 2 * parent + 1;
+	while (child < size)
+	{
+		//找出左右孩子哪个更大并更新
+		if (child + 1 < size && arr[child] < arr[child+1])
+		{
+			child++;
+		}
+		if (arr[child] > arr[parent])
+		{
+			Swap(&arr[child], &arr[parent]);
+			parent = child;
+			child = 2 * parent + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+//升序建大堆
+void Test_HeapSort(int* arr, int size)
+{
+	for (int i = (size - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(arr, size, i);
+	}
+	int end = size - 1;
+	while (end > 0)
+	{
+		Swap(&arr[0], &arr[end]);
+		AdjustDown(arr, end, 0);
+		end--;
+	}
+}
+
+int main()
+{
+	int arr[] = { 10,9,8,7,6,5,4,3,2,1 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	//Test_InsertSort(arr, size);
+	//Test_ShellSort(
+	// arr, size);
+	//Test_SelectSort(arr, size);
+	Test_HeapSort(arr, size);
+	Print(arr, size);
+	return 0;
+}
